e24d69505f7d94f34b35986047aab1646ce94f54
LuisCarrascosa <luis.carrascosa.cabrera@gmail.com>
2019-05-20 10:49:31 +0200

    eliminación de temas personales

--- aws-lambda-canary/venv/my_handler.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def handler(event, context):
-    r = requests.get('https://www.fernandomc.com')
-    print(r.text)
-    if r.status_code == '200':
-        return 'Success!'
-    else:
-        return 'FAILURE!!!'
